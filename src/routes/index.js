4a15330b96375a1b655273512addfc34d6ae726e
bestboom <certseek@gmail.com>
2021-10-29 22:56:29 +0500

    up

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path')
+const cookieParser = require('cookie-parser');
+
+const router = express.Router();
+
+
+router.use(cookieParser());
+
+
+router.get('', async (req, res) => {res.render('index')})
+router.get('/farming', async (req, res) => {res.render('farming')})
+router.get('/staking', async (req, res) => {res.render('staking')})
+router.get('/faq', async (req, res) => {res.render('faq')})
+module.exports = router;
