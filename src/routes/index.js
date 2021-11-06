c304d846080d1cc7b6f6b18b333e9dcd67e89801
bestboom <certseek@gmail.com>
2021-11-06 23:18:56 +0500

    up

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,5 +12,5 @@ router.get('', async (req, res) => {res.render('index')})
 router.get('/farming', async (req, res) => {res.render('farming')})
 router.get('/staking', async (req, res) => {res.render('staking')})
 router.get('/faq', async (req, res) => {res.render('faq')})
-router.get('/about', async (req, res) => {res.render('about')})
+router.get('/bridge', async (req, res) => {res.render('bridge')})
 module.exports = router;
